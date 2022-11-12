4a39c2b6b5936809481bc305fede24a4d89b81da
Igor Peronico <igormtpr@gmail.com>
2022-11-12 15:04:34 -0300

    Final

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
+import br.com.dio.desafio.dominio.Bootcamp;
 import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Dev;
 import br.com.dio.desafio.dominio.Mentoria;
 
 import java.time.LocalDate;
@@ -21,9 +23,43 @@ public class Main {
         mentoria.setDescricao("Descrição Mentoria Java");
         mentoria.setData(LocalDate.now());
 
-        System.out.println(curso1);
+        /*System.out.println(curso1);
         System.out.println(curso2);
-        System.out.println(mentoria);
+        System.out.println(mentoria);*/
 
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("Descrição Bootcamp Java Developer");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria);
+
+        Dev devIgor = new Dev();
+        devIgor.setNome("Igor Peronico");
+        devIgor.inscreverBootcamp(bootcamp);
+        System.out.println("Conteudos Inscritos de Igor: " + devIgor.getConteudosInscritos());
+        devIgor.progredir();
+        System.out.println("-");
+        System.out.println("Conteudos Inscritos de Igor: " + devIgor.getConteudosInscritos());
+        System.out.println("Conteudos Concluídos de Igor: " + devIgor.getConteudosConcluidos());
+        devIgor.progredir();
+        System.out.println("-");
+        System.out.println("Conteudos Inscritos de Igor: " + devIgor.getConteudosInscritos());
+        System.out.println("Conteudos Concluídos de Igor: " + devIgor.getConteudosConcluidos());
+        System.out.println("XP Igor: " + devIgor.calcularTotalXp());
+
+        System.out.println("--------------------------------");
+
+        Dev devMarcilia = new Dev();
+        devMarcilia.setNome("Marcilia Carla");
+        devMarcilia.inscreverBootcamp(bootcamp);
+        System.out.println("Conteudos Inscritos de Marcilia: " + devMarcilia.getConteudosInscritos());
+        devMarcilia.progredir();
+        devMarcilia.progredir();
+        devMarcilia.progredir();
+        System.out.println("-");
+        System.out.println("Conteudos Inscritos de Marcilia: " + devMarcilia.getConteudosInscritos());
+        System.out.println("Conteudos Concluídos de Marcilia: " + devMarcilia.getConteudosConcluidos());
+        System.out.println("XP Marcilia: " + devMarcilia.calcularTotalXp());
     }
 }
